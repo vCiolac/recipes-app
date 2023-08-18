aabcfece85d503217b979a8485eec6a79ad94752
¨David <davidl1199@hotmail.com>
2023-08-18 15:55:40 -0300

    feat: creating components folder and Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/Login/Login';
 
 function App() {
   return (
-    <div>Starting Project!</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={ Login } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
