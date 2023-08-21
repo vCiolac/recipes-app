bcbeee8d78716be678f1d2e54257f9ce122bb4b6
Leonardo Lelis <leolelislopes@yahoo.com.br>
2023-08-21 17:31:05 -0300

    implementação do botão de busca que, ao ser clicado, permita a visualização da barra de busca ou a esconda

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import recipeAppIcon from '../../images/ícone-Recipes-app.png';
 
 type HeaderProps = {
@@ -11,15 +12,21 @@ function Header({ title,
   searchIcon = undefined,
   profileIcon = undefined,
   iconTitle }: HeaderProps) {
+  const [hideSearch, setHideSearch] = useState(false);
+
   return (
     <header>
       <img src={ recipeAppIcon } alt="Ícone Recipes App" />
       <h1>Recipes App</h1>
-      {searchIcon && <img
-        data-testid="search-top-btn"
-        src={ searchIcon }
-        alt="Lupa de Pesquisa"
-      />}
+      {searchIcon
+      && (
+        <button onClick={ () => setHideSearch(!hideSearch) }>
+          <img
+            data-testid="search-top-btn"
+            src={ searchIcon }
+            alt="Lupa de Pesquisa"
+          />
+        </button>)}
       {profileIcon && <img
         data-testid="profile-top-btn"
         src={ profileIcon }
@@ -27,6 +34,8 @@ function Header({ title,
       />}
       <img src={ iconTitle } alt="Ícone do Título" />
       <h1 data-testid="page-title">{title}</h1>
+      {hideSearch
+      && <input type="text" placeholder="Search" data-testid="search-input" />}
     </header>
   );
 }
