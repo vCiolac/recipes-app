7fa8dd981e01a663413f2a7664791a362e13abfa
Leonardo Lelis <leolelislopes@yahoo.com.br>
2023-08-21 18:24:54 -0300

    testes para o componente Header

--- src/tests/Header.test.tsx
+++ src/tests/Header.test.tsx
@@ -1,15 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Meals from '../Pages/Meals/Meals';
+import { renderWithRouter } from '../utils/renderWithRouter';
+import App from '../App';
 
 describe('Testes referentes ao Header', () => {
   test('Testa se o header possui o nome Recipes App e seu respectivo ícone de imagem.', () => {
-    render(
-      <BrowserRouter>
-        <Meals />
-      </BrowserRouter>,
-    );
+    renderWithRouter(<Meals />);
 
     const appTitle = screen.getByRole('heading', { name: /recipes app/i });
     expect(appTitle).toBeInTheDocument();
@@ -18,14 +15,22 @@ describe('Testes referentes ao Header', () => {
   });
 
   test('Testa se ao clicar no botão de perfil o usuário é redirecionado para a rota /profile.', async () => {
-    render(
-      <BrowserRouter>
-        <Meals />
-      </BrowserRouter>,
-    );
+    renderWithRouter(<App />, { route: '/profile' });
 
     const iconeDePerfil = screen.getByRole('img', { name: /ícone de perfil/i });
     await userEvent.click(iconeDePerfil);
-    expect(screen.findByRole('heading', { name: /profile/i }));
+    expect(await screen.findByRole('heading', { name: /profile/i })).toBeInTheDocument();
   });
+});
+
+test('Testa o botão de busca que, ao ser clicado, permite a visualização da barra de busca ou a esconda', async () => {
+  renderWithRouter(<Meals />);
+  const searchBtn = screen.getByRole('img', { name: /lupa de pesquisa/i });
+  await userEvent.click(searchBtn);
+  const searchBar = screen.getByPlaceholderText(/search/i);
+
+  expect(searchBar).toBeInTheDocument();
+
+  await userEvent.click(searchBtn);
+  expect(searchBar).not.toBeInTheDocument();
 });
