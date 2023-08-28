755d4b111ceb6a85e264737bd22e48b668308fee
Beatriz Reis <beatriznunes777@gmail.com>
2023-08-28 19:55:40 -0300

    requisitos 44 a 49

--- src/tests/DoneRecipes.test.tsx
+++ src/tests/DoneRecipes.test.tsx
@@ -1,9 +1,10 @@
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { renderWithRouter } from './helpers/renderWithRouter';
 import App from '../App';
 
 describe('Testando a página de "Done Recipes"', () => {
-  test('testando se os elementos são renderizados na tela', () => {
+  test('testando se os elementos são renderizados na tela', async () => {
     renderWithRouter(<App />, { route: '/done-recipes' });
 
     const h1 = screen.getByRole('heading', { level: 1, name: 'Done Recipes' });
@@ -16,5 +17,8 @@ describe('Testando a página de "Done Recipes"', () => {
     expect(FilterAll).toBeInTheDocument();
     expect(FilterMeals).toBeInTheDocument();
     expect(FilterDrinks).toBeInTheDocument();
+    await userEvent.click(FilterMeals);
+    await userEvent.click(FilterAll);
+    await userEvent.click(FilterDrinks);
   });
 });
