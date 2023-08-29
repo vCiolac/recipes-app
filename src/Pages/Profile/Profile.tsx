9b25cb3717e3a78ee46b5280c608a8b682101384
Victor Ciolac <victor.bcfraga@gmail.com>
2023-08-29 16:22:46 -0300

    Pagina de login completa

--- src/Pages/Profile/Profile.tsx
+++ src/Pages/Profile/Profile.tsx
@@ -1,8 +1,29 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import profileIcon from '../../images/profileIcon.svg';
+import useLocalStorage from '../../Hooks/useLocalStorage';
 
 function Profile() {
+  const {
+    localStorageValue: userEmail,
+    updateValue: setUserEmail,
+  } = useLocalStorage('user', {} as any);
+
+  const navigate = useNavigate();
+  const handleDoneRecipes = () => {
+    navigate('/done-recipes');
+  };
+  const handleRecipesFavorite = () => {
+    navigate('/favorite-recipes');
+  };
+  const handleLogout = () => {
+    setUserEmail({});
+    localStorage.clear();
+    navigate('/');
+  };
+
   return (
     <>
       <Header
@@ -10,7 +31,28 @@ function Profile() {
         profileIcon={ profileIcon }
         iconTitle={ profileIcon }
       />
-      <Footer />
+      <div>
+        <h1 data-testid="profile-email">{ userEmail.email }</h1>
+        <button
+          onClick={ handleDoneRecipes }
+          data-testid="profile-done-btn"
+        >
+          Done Recipes
+        </button>
+        <button
+          onClick={ handleRecipesFavorite }
+          data-testid="profile-favorite-btn"
+        >
+          Favorite Recipes
+        </button>
+        <button
+          onClick={ handleLogout }
+          data-testid="profile-logout-btn"
+        >
+          Logout
+        </button>
+        <Footer />
+      </div>
     </>
   );
 }
