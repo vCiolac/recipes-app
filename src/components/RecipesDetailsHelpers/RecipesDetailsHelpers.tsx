71536f246e565a4dbd6a4ec493e8533f1317bc5f
Victor Ciolac <victor.bcfraga@gmail.com>
2023-09-05 11:25:58 -0300

    add random carousel

--- src/components/RecipesDetailsHelpers/RecipesDetailsHelpers.tsx
+++ src/components/RecipesDetailsHelpers/RecipesDetailsHelpers.tsx
@@ -1,8 +1,8 @@
 export function getSixRecipes(isMeal: boolean, mealInf: any, drinkInf: any) {
   const recipesToDisplay = !isMeal ? mealInf : drinkInf;
   if (recipesToDisplay?.length >= 6) {
-    // const randomRecipes = recipesToDisplay.slice().sort(() => Math.random() - 0.5);
-    return recipesToDisplay.slice(0, 6);
+    const randomRecipes = recipesToDisplay.slice().sort(() => Math.random() - 0.5);
+    return randomRecipes;
   }
   return recipesToDisplay;
 }
